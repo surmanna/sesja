5264801b7c657759adcbd8615a091c0fe949a9d2
unknown <surmaannalo@gmail.com>
2020-02-29 12:23:28 +0100

    dodalam

---
+++ src/com/company/Git.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Git extends Prism {
+    String sesja;
+
+    @Override
+    double evaluateVolume() {
+        return 0;
+    }
+
+    @Override
+    double evaluatefield() {
+        return 0;
+    }
+
+    @Override
+    double evaluateside_field() {
+        return 0;
+    }
+
+    @Override
+    double evaluateBottom() {
+        return 0;
+    }
+
+    @Override
+    String getFigureInfo() {
+        return null;
+    }
+}
