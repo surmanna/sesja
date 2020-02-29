2a576037f50203a7097890082300a78756b24abb
unknown <surmaannalo@gmail.com>
2020-02-29 13:45:22 +0100

    gitjeden

---
+++ src/com/company/Gitjeden.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Gitjeden extends Prism {
+    String zaliczenie;
+
+
+    @Override
+    double evaluateVolume() {
+        return 0;
+    }
+
+    @Override
+    double evaluatefield() {
+        return 0;
+    }
+
+    @Override
+    double evaluateside_field() {
+        return 0;
+    }
+
+    @Override
+    double evaluateBottom() {
+        return 0;
+    }
+
+    @Override
+    String getFigureInfo() {
+        return null;
+    }
+}
